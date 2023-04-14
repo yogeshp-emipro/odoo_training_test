1b7385e2d018217b23af69ca72c83edfb64aec36
Yogesh Pandey <yogeshp@emiprotechnologies.com>
2023-04-14 19:56:55 +0530

    [ADD] Adding new file to sale_commission_ept_module

--- sale_commission_ept/models/account_invoice.py
+++ sale_commission_ept/models/account_invoice.py
@@ -3,4 +3,4 @@ from odoo import models,fields
 class AccountInvoice(models.Model):
     _inherit='account.move'
 
-    commission_id=fields.Many2one(comodel_name='sales.commission.ept',string='Commission',help='id of the commission ') 
+    commission_id=fields.Many2one(comodel_name='sales.commission.ept',string='Commission',help='id of the commission ')
